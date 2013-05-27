7f870a2d91d5a947ea94f95de5f08338b717ae36
Dominik Bathon <dominik.bathon@gmail.com>
2013-05-28 00:11:31 +0200

    improve EntryService#query()

--- src/main/java/dbathon/web/taggedstuff/service/EntryService.java
+++ src/main/java/dbathon/web/taggedstuff/service/EntryService.java
@@ -1,7 +1,10 @@
 package dbathon.web.taggedstuff.service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 import javax.ejb.ConcurrencyManagement;
 import javax.ejb.ConcurrencyManagementType;
 import javax.ejb.Singleton;
@@ -21,17 +24,27 @@ public class EntryService extends AbstractEntityService<Entry> {
 
     final String queryParam = queryParameters.get("query", String.class);
     if (queryParam != null) {
-      // TODO: improve conditions...
+      final Set<String> positiveTags = new HashSet<>();
+      final Set<String> negativeTags = new HashSet<>();
       for (final String queryPart : Util.splitToTrimmedStrings(queryParam)) {
         if (queryPart.startsWith("+")) {
-          builder.add("exists (select t from e.tags t where t.id = ?)", queryPart.substring(1));
+          positiveTags.add(queryPart.substring(1));
         }
         else if (queryPart.startsWith("-")) {
-          builder.add("not exists (select t from e.tags t where t.id = ?)", queryPart.substring(1));
+          negativeTags.add(queryPart.substring(1));
         }
         else {
-          builder.add("e.title like ?", "%" + queryPart + "%");
+          final String likeString = ("%" + queryPart + "%").toLowerCase(Locale.ROOT);
+          builder.add("lower(e.title) like ? or lower(e.url) like ? or lower(e.body) like ?",
+              likeString, likeString, likeString);
         }
+      }
+      if (!positiveTags.isEmpty()) {
+        builder.add("e in (select ee from Entry ee join ee.tags t where t.id in (?))", positiveTags);
+      }
+      if (!negativeTags.isEmpty()) {
+        builder.add("e not in (select ee from Entry ee join ee.tags t where t.id in (?))",
+            negativeTags);
       }
     }
 
