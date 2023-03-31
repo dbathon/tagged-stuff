661da177317d315532926c8b0022e476e2370efa
Dominik Bathon <dominik.bathon@gmail.com>
2023-04-01 00:43:03 +0200

    Implement write/readCompressedFloat64()

---
+++ src/data/shared/compressedFloat64.spec.ts
@@ -0,0 +1,135 @@
+import { describe, expect, test } from "vitest";
+import { readCompressedFloat64, writeCompressedFloat64 } from "./compressedFloat64";
+
+describe("read/writeCompressedFloat64()", () => {
+  test("throws on offset is out of bounds", () => {
+    const array = new Uint8Array(9);
+    const zeroArray = new Uint8Array(9);
+    const tests: [number, number, boolean?][] = [
+      [9, 32, true],
+      [10, 32, true],
+      [-1, 32, true],
+      [8, 32],
+      [7, 320],
+      [6, 320000],
+      [5, 32000000000],
+    ];
+    for (const [offset, value, skipMinusOne] of tests) {
+      array.set(zeroArray);
+      expect(() => writeCompressedFloat64(array, offset, value)).toThrowError("offset is out of bounds");
+      // no modification
+      expect(array).toEqual(zeroArray);
+
+      if (!skipMinusOne) {
+        expect(() => writeCompressedFloat64(array, offset - 1, value)).not.toThrowError();
+      }
+    }
+  });
+
+  const testValues = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    8,
+    16,
+    24,
+    100,
+    250,
+    600,
+    2000,
+    56789,
+    2309543,
+    1.5,
+    1.2,
+    1.23,
+    1.234,
+    1.2345,
+    1.23456,
+    1.234567,
+    1.2345678,
+    1.23456789,
+    1.234567891,
+    1.2345678912,
+    1.23456789123,
+    1.234567891234,
+    1.2345678912345,
+    1.23456789123456,
+    1.234567891234567,
+    1.2345678912345678,
+    1.23456789123456789,
+    1.234567891234567891,
+    1.234567891234567891,
+    12.34567891234567891,
+    123.4567891234567891,
+    1234.567891234567891,
+    12345.67891234567891,
+    123456.7891234567891,
+    1234567.891234567891,
+    12345678.91234567891,
+    123456789.1234567891,
+    1234567891.234567891,
+    12345678912.34567891,
+    123456789123.4567891,
+    1234567891234.567891,
+    12345678912345.67891,
+    123456789123456.7891,
+    1234567891234567.891,
+    12345678912345678.91,
+    123456789123456789.1,
+    1234567891234567891.0,
+    1234567891234567891e10,
+    1234567891234567891e20,
+    1234567891234567891e40,
+    1234567891234567891e80,
+    1234567891234567891e160,
+    1234567891234567891e300,
+    1234567891234567891e-10,
+    1234567891234567891e-20,
+    1234567891234567891e-40,
+    1234567891234567891e-80,
+    1234567891234567891e-160,
+    1234567891234567891e-300,
+    NaN,
+  ];
+
+  function testRoundtrip(testValue: number) {
+    const array = new Uint8Array(11);
+    const writeLength = writeCompressedFloat64(array, 1, testValue);
+    expect(writeLength).toBeGreaterThanOrEqual(1);
+    expect(writeLength).toBeLessThanOrEqual(9);
+
+    // check that other array values are unchanged
+    expect(array[0]).toBe(0);
+    for (let i = writeLength + 1; i < array.length; i++) {
+      expect(array[i]).toBe(0);
+    }
+
+    const { value: readValue, length: readLength } = readCompressedFloat64(array, 1);
+    expect(readValue).toBe(testValue);
+    expect(readLength).toBe(writeLength);
+  }
+
+  test("reads the written value", () => {
+    for (const testValue of testValues) {
+      testRoundtrip(testValue);
+      testRoundtrip(-testValue);
+    }
+  });
+
+  test("all exponents", () => {
+    const dataView = new DataView(new ArrayBuffer(8));
+    for (let exponent = 0; exponent < 2048; exponent++) {
+      const part1 = exponent << 20;
+      dataView.setUint32(0, part1);
+      testRoundtrip(dataView.getFloat64(0));
+      dataView.setUint32(0, part1 | (1 << 19));
+      testRoundtrip(dataView.getFloat64(0));
+      dataView.setUint32(0, part1 | 0x80000000);
+      testRoundtrip(dataView.getFloat64(0));
+      dataView.setUint32(0, part1 | 0x80000000 | (1 << 19));
+      testRoundtrip(dataView.getFloat64(0));
+    }
+  });
+});
---
+++ src/data/shared/compressedFloat64.ts
@@ -0,0 +1,135 @@
+const EXPONENT_BITS = 11;
+const FRACTION_BITS_IN_PART1 = 20;
+
+const EXPONENT_MASK = (1 << EXPONENT_BITS) - 1;
+
+const scratchDataView = new DataView(new ArrayBuffer(8));
+
+/**
+ * Writes the given float64 number to the array starting at offset. Writes 1 to 9 bytes depending on the number.
+ *
+ * Use readCompressedFloat64() to read numbers written with this function.
+ *
+ * @returns the number of written bytes
+ */
+export function writeCompressedFloat64(array: Uint8Array, offset: number, value: number): number {
+  scratchDataView.setFloat64(0, value);
+  const part1 = scratchDataView.getUint32(0);
+  const part2 = scratchDataView.getUint32(4);
+
+  const sign = part1 >>> 31;
+  const exponent = (part1 >>> FRACTION_BITS_IN_PART1) & EXPONENT_MASK;
+  // convert the exponent so that values close to 1023 are close to 0 to get better compression for common numbers
+  const transformedExponent = exponent <= 1023 ? ((1023 - exponent) << 1) | 1 : (exponent - 1024) << 1;
+  // we need 2 bytes for the "exponent part" if the exponent is longer than 3 bits
+  const twoByteExponent = transformedExponent > 0b111;
+
+  let fractionBytes: number;
+  if (part2) {
+    if (part2 & 0xfff) {
+      fractionBytes = part2 & 0xf ? 7 : 6;
+    } else if (part2 & 0xffff000) {
+      fractionBytes = part2 & 0xff000 ? 5 : 4;
+    } else {
+      fractionBytes = 3;
+    }
+  } else {
+    if (part1 & 0xfff) {
+      fractionBytes = part1 & 0xf ? 3 : 2;
+    } else {
+      fractionBytes = part1 & 0xff000 ? 1 : 0;
+    }
+  }
+
+  const length = (twoByteExponent ? 2 : 1) + fractionBytes;
+  if (offset < 0 || offset + length > array.length) {
+    throw new Error("offset is out of bounds");
+  }
+
+  array[offset] =
+    (twoByteExponent ? 0x80 : 0) |
+    (sign << 6) |
+    (fractionBytes << 3) |
+    (twoByteExponent ? transformedExponent >>> 8 : transformedExponent);
+  if (twoByteExponent) {
+    array[offset + 1] = transformedExponent & 0xff;
+  }
+
+  if (fractionBytes > 0) {
+    const fractionStart = offset + (twoByteExponent ? 2 : 1);
+    // use switch with fall through
+    switch (fractionBytes) {
+      case 7:
+        array[fractionStart + 6] = (part2 & 0xf) << 4;
+      case 6:
+        array[fractionStart + 5] = (part2 & 0xff0) >>> 4;
+      case 5:
+        array[fractionStart + 4] = (part2 & 0xff000) >>> 12;
+      case 4:
+        array[fractionStart + 3] = (part2 & 0xff00000) >>> 20;
+      case 3:
+        array[fractionStart + 2] = ((part1 & 0xf) << 4) | ((part2 & 0xf0000000) >>> 28);
+      case 2:
+        array[fractionStart + 1] = (part1 & 0xff0) >>> 4;
+      case 1:
+        array[fractionStart] = (part1 & 0xff000) >>> 12;
+    }
+  }
+
+  return length;
+}
+
+/**
+ * Reads a compressed float64 number written by writeCompressedFloat64().
+ *
+ * @param array
+ * @param offset
+ * @returns the read value and the number of bytes consumed (length)
+ */
+export function readCompressedFloat64(array: Uint8Array, offset: number): { value: number; length: number } {
+  const byte0 = array[offset];
+  const twoByteExponent = (byte0 & 0x80) > 0;
+  const sign = byte0 & 0x40 ? 0x80000000 : 0;
+  const fractionBytes = (byte0 >>> 3) & 0b111;
+
+  let transformedExponent;
+  if (twoByteExponent) {
+    transformedExponent = ((byte0 & 0b111) << 8) | array[offset + 1];
+  } else {
+    transformedExponent = byte0 & 0b111;
+  }
+  const exponent = transformedExponent & 1 ? 1023 - (transformedExponent >>> 1) : (transformedExponent >>> 1) + 1024;
+
+  let part1 = sign | (exponent << FRACTION_BITS_IN_PART1);
+  let part2 = 0;
+
+  if (fractionBytes > 0) {
+    const fractionStart = offset + (twoByteExponent ? 2 : 1);
+    // use switch with fall through
+    switch (fractionBytes) {
+      case 7:
+        part2 |= array[fractionStart + 6] >>> 4;
+      case 6:
+        part2 |= array[fractionStart + 5] << 4;
+      case 5:
+        part2 |= array[fractionStart + 4] << 12;
+      case 4:
+        part2 |= array[fractionStart + 3] << 20;
+      case 3:
+        const fractionByte2 = array[fractionStart + 2];
+        part2 |= fractionByte2 << 28;
+        part1 |= fractionByte2 >>> 4;
+      case 2:
+        part1 |= array[fractionStart + 1] << 4;
+      case 1:
+        part1 |= array[fractionStart] << 12;
+    }
+  }
+
+  scratchDataView.setUint32(0, part1);
+  scratchDataView.setUint32(4, part2);
+  return {
+    value: scratchDataView.getFloat64(0),
+    length: (twoByteExponent ? 2 : 1) + fractionBytes,
+  };
+}
