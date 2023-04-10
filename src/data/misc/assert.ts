9b2a2d0690b7d825df6d34a29c390d3090fecaf6
Dominik Bathon <dominik.bathon@gmail.com>
2023-04-10 23:19:36 +0200

    Change assert() to not include the messages in the production build

--- src/data/misc/assert.ts
+++ src/data/misc/assert.ts
@@ -1,6 +1,10 @@
 export function assert(checkCondition: unknown, errorMessage?: string | (() => string)): asserts checkCondition {
   if (!checkCondition) {
-    const message = typeof errorMessage === "string" ? errorMessage : errorMessage?.();
-    throw new Error(message);
+    if (import.meta.env.PROD) {
+      throw new Error();
+    } else {
+      const message = typeof errorMessage === "string" ? errorMessage : errorMessage?.();
+      throw new Error(message);
+    }
   }
 }
