4edd680cad4a56a9d15e8c39a689cdbaba4a8b65
Dominik Bathon <dominik.bathon@gmail.com>
2025-01-13 00:44:04 +0100

    Implement fine-grained updates in useJsonQuery()
    Try to do the minimal changes to an existing result array to allow fine
    grained updates in the components.

--- packages/app/src/pages/json-store/useJsonQuery.ts
+++ packages/app/src/pages/json-store/useJsonQuery.ts
@@ -1,12 +1,103 @@
-import { computed, type ComputedRef, onScopeDispose, ref } from "vue";
+import { onScopeDispose, type Ref, ref, watch } from "vue";
 import { queryJson } from "../../data/json/jsonStore";
 import { PageStore } from "../../data/page-store/PageStore";
 import { type QueryParameters } from "../../data/json/queryTypes";
 
+function extractId(value: unknown): number | undefined {
+  if (typeof value === "object" && value !== null) {
+    const id = (value as Record<string, unknown>).id;
+    if (typeof id === "number") {
+      return id;
+    }
+  }
+  return undefined;
+}
+
+function buildIdMap(array: unknown[]): Map<number, unknown> | undefined {
+  let result: Map<number, unknown> | undefined = undefined;
+  for (const entry of array) {
+    const id = extractId(entry);
+    if (id === undefined) {
+      // not all entries have an id
+      return undefined;
+    }
+    if (!result) {
+      // lazy init
+      result = new Map();
+    }
+    if (result.has(id)) {
+      // not all ids are distinct
+      return undefined;
+    }
+    result.set(id, entry);
+  }
+  return result;
+}
+
+function mergeInto(existingValue: unknown, newValue: unknown): unknown {
+  if (existingValue === newValue) {
+    return existingValue;
+  }
+  if (typeof existingValue === "object" && typeof newValue === "object") {
+    if (Array.isArray(newValue)) {
+      if (Array.isArray(existingValue)) {
+        const length = newValue.length;
+        // merge array values
+        if (length) {
+          const idMap = buildIdMap(existingValue);
+          if (idMap) {
+            // reuse existing values by id
+            for (let i = 0; i < length; i++) {
+              const newEntry = newValue[i];
+              const id = extractId(newEntry);
+              const existingEntry = id !== undefined ? idMap.get(id) : undefined;
+              const mergedEntry = mergeInto(existingEntry, newEntry);
+              if (existingValue[i] !== mergedEntry) {
+                existingValue[i] = mergedEntry;
+              }
+            }
+          } else {
+            // reuse existing values by position
+            for (let i = 0; i < length; i++) {
+              const existingEntry = existingValue[i];
+              const mergedEntry = mergeInto(existingEntry, newValue[i]);
+              if (existingEntry !== mergedEntry) {
+                existingValue[i] = mergedEntry;
+              }
+            }
+          }
+        }
+        existingValue.length = length;
+        return existingValue;
+      }
+    } else if (existingValue && newValue && !Array.isArray(existingValue)) {
+      // both existingValue and newValue are objects
+      const existingRecord = existingValue as Record<string, unknown>;
+      const newRecord = newValue as Record<string, unknown>;
+      const unhandledKeys = new Set(Object.keys(existingRecord));
+      for (const [key, newEntryValue] of Object.entries(newRecord)) {
+        if (newEntryValue !== undefined) {
+          const existingEntryValue = existingRecord[key];
+          const mergedEntryValue = mergeInto(existingEntryValue, newEntryValue);
+          if (existingEntryValue !== mergedEntryValue) {
+            existingRecord[key] = mergedEntryValue;
+          }
+          unhandledKeys.delete(key);
+        }
+      }
+      unhandledKeys.forEach((key) => delete existingRecord[key]);
+      return existingRecord;
+    }
+  }
+
+  // the two values cannot be merged, just return newValue
+  return newValue;
+}
+
 export function useJsonQuery<T extends object>(
   pageStore: PageStore,
   queryParameters: () => QueryParameters
-): ComputedRef<T[] | false> {
+): Ref<T[] | false> {
   // this ref is used to trigger a recompute
   const invalidateToggle = ref(false);
   let lastInvalidateToggleValue = false;
@@ -19,14 +110,30 @@ export function useJsonQuery<T extends object>(
     pageReadsRecorder(() => {});
   });
 
-  return computed(() => {
-    // read invalidateToggle here to recompute if it changes
-    lastInvalidateToggleValue = invalidateToggle.value;
-    const currentParameters = queryParameters();
-    const label = "recompute " + JSON.stringify(currentParameters);
-    console.time(label);
-    const result = pageReadsRecorder((pageAccess) => queryJson<T>(pageAccess, currentParameters));
-    console.timeEnd(label);
-    return result;
-  });
+  const result: Ref<T[] | false> = ref(false);
+
+  watch(
+    [invalidateToggle, queryParameters],
+    ([invalidateToggleValue, currentParameters]) => {
+      lastInvalidateToggleValue = invalidateToggleValue;
+      const label = JSON.stringify(currentParameters);
+      console.time("recompute " + label);
+      const queryResult = pageReadsRecorder((pageAccess) => queryJson<T>(pageAccess, currentParameters));
+      console.timeEnd("recompute " + label);
+      if (!result.value) {
+        result.value = queryResult;
+      } else {
+        if (queryResult) {
+          console.time("merge " + label);
+          mergeInto(result.value, queryResult);
+          console.timeEnd("merge " + label);
+        } else {
+          // just keep the existing result if the query goes back to false
+        }
+      }
+    },
+    { immediate: true }
+  );
+
+  return result;
 }
