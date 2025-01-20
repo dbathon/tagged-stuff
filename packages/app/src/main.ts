1c4c72d506c2c50d0c106ba34bb254d6d36d17bf
Dominik Bathon <dominik.bathon@gmail.com>
2025-01-20 02:00:38 +0100

    Allow tree shaking of assert messages

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -1,6 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router";
+import { enableMessagesForAssert } from "shared-util";
+
+if (import.meta.env.DEV) {
+  enableMessagesForAssert();
+}
 
 const app = createApp(App);
 app.use(router);
--- packages/shared-util/src/misc/assert.ts
+++ packages/shared-util/src/misc/assert.ts
@@ -1,8 +1,20 @@
-// TODO: remove message parameter entirely?!
-export function assert(checkCondition: unknown, _errorMessage?: string | (() => string)): asserts checkCondition {
+let messagesEnabled = false;
+
+export function assert(checkCondition: unknown, errorMessage?: string | (() => string)): asserts checkCondition {
   if (!checkCondition) {
-    throw new Error();
-    // const message = typeof errorMessage === "string" ? errorMessage : errorMessage?.();
-    // throw new Error(message);
+    if (messagesEnabled) {
+      const message = typeof errorMessage === "string" ? errorMessage : errorMessage?.();
+      throw new Error(message);
+    } else {
+      throw new Error();
+    }
   }
+}
+
+/**
+ * This can be called in "dev builds" to "enable" the errorMessage parameter for assert(). If it is not called, then it
+ * is possible to omit all the messages from the build via tree shaking.
+ */
+export function enableMessagesForAssert() {
+  messagesEnabled = true;
 }
