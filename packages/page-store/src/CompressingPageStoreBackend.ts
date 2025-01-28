dd0e117f253b116ea9862b9309119d2471cec031
Dominik Bathon <dominik.bathon@gmail.com>
2025-01-28 23:59:26 +0100

    Use deflate-raw instead of gzip in CompressingPageStoreBackend

--- packages/page-store/src/CompressingPageStoreBackend.ts
+++ packages/page-store/src/CompressingPageStoreBackend.ts
@@ -2,17 +2,19 @@ import { DataTransformingPageStoreBackend } from "./DataTransformingPageStoreBac
 import type { PageStoreBackend } from "./PageStoreBackend";
 
 const NO_COMPRESSION = 0;
+// gzip is currently never written, since it has an overhead of 18 bytes per page
 const GZIP_COMPRESSION = 1;
+const DEFLATE_RAW_COMPRESSION = 2;
 
 async function compress(data: Uint8Array): Promise<Uint8Array> {
   const arrayBuffer = await new Response(
-    new Blob([data]).stream().pipeThrough(new CompressionStream("gzip")),
+    new Blob([data]).stream().pipeThrough(new CompressionStream("deflate-raw")),
   ).arrayBuffer();
   if (arrayBuffer.byteLength <= data.byteLength) {
     // this should be the very common case
     const result = new Uint8Array(arrayBuffer.byteLength + 1);
     result.set(new Uint8Array(arrayBuffer), 0);
-    result[result.length - 1] = GZIP_COMPRESSION;
+    result[result.length - 1] = DEFLATE_RAW_COMPRESSION;
     return result;
   } else {
     // this should happen very rarely, just use the uncompressed data if it is not compressible
@@ -27,9 +29,10 @@ async function decompress(data: Uint8Array): Promise<Uint8Array> {
   const type = data[data.length - 1];
   if (type === NO_COMPRESSION) {
     return data.subarray(0, data.length - 1);
-  } else if (type === GZIP_COMPRESSION) {
+  } else if (type === GZIP_COMPRESSION || type === DEFLATE_RAW_COMPRESSION) {
+    const format = type === GZIP_COMPRESSION ? "gzip" : "deflate-raw";
     const arrayBuffer = await new Response(
-      new Blob([data.subarray(0, data.length - 1)]).stream().pipeThrough(new DecompressionStream("gzip")),
+      new Blob([data.subarray(0, data.length - 1)]).stream().pipeThrough(new DecompressionStream(format)),
     ).arrayBuffer();
     return new Uint8Array(arrayBuffer);
   } else {
